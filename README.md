49ce06f464a7816645313102461140ebae3b23e2
semiromid <startpascal@mail.ru>
2016-02-20 17:30:21 +0200

    ver 0.1

--- README.md
+++ README.md
@@ -1,2 +1,183 @@
 # gulp-control-version
-gulp-control-version for control version file names
+
+> [gulp-control-version](https://github.com/semiromid/gulp-control-version) is a gulp plugin to update modules easily.
+
+
+
+[En]
+gulp-control-version is a plugin to rename file with the addition of version.
+
+[Ru]
+gulp-control-version переименовывает конечный файл, добавляя к нему префикс, с уникальным неповторяющимся индексом. 
+
+
+
+## Install
+
+```shell
+npm install  gulp-control-version --save-dev
+```
+
+## Usage
+
+Then, add it to your gulpfile.js:
+
+```javascript
+var conver = require('gulp-control-version');
+
+
+
+//-----------gulp-control-version-----------
+gulp.task('v-concat_css_1',  function () { //
+    conver.rewrite({
+                   resdir: "basic/components/widgets/assets/css/",
+                   namefile: "-general_style-min.css", 
+                   dirrefactor: ["basic/assets/", ["NavbarAsset.php","NavbarAsset_transparent.php"]], 
+                   namerefactor: "-general_style-min",
+                   codname: "utf-8"
+                  });
+});
+//------------------------------------------
+```
+
+
+## Example
+
+```javascript
+var conver = require('gulp-control-version');
+//-----------CONCAT-------------------------
+gulp.task('concat_js_1', ['compress-js'], function () {
+    return gulp.src(['./src/js/js_min/navbar_animate_button-min.js' , './src/js/js_min/navbar_mobilemenu-min.js'])
+      .pipe(concat(conver('./basic/components/widgets/assets/js/','-navbar.js')+'-navbar.js'))
+      .pipe(gulp.dest("./basic/components/widgets/assets/js/"));
+});
+//------------------------------------------
+
+//-----------gulp-control-version-----------
+gulp.task('v-concat_js_1', ['concat_js_1'], function () { 
+  conver.rewrite({
+                resdir: "basic/components/widgets/assets/js/",
+                namefile: "-navbar.js", 
+                dirrefactor: ["basic/assets/", ["NavbarAsset.php"]]
+              });
+});
+//------------------------------------------
+```
+
+
+OR
+
+## 1
+
+```javascript
+basic/components/widgets/assets/css/123423198-general_style-min.сss
+```
+
+## 2
+
+```javascript
+var conver = require('gulp-control-version');
+//-----------CSSNANO-------------------------
+gulp.task('cssnano', function () {
+    return gulp.src('src/css/css/**/*.css')
+        .pipe(nano({
+            autoprefixer: {
+                browsers: ['last 16 versions'],
+                add: true
+            }
+        }))
+        //gulp - rename
+        //-----------------------------
+        .pipe(rename({
+              basename: conver('./basic/components/widgets/assets/css/','-general_style-min.сss')+"aloha",
+            }))
+        //-----------------------------
+        .pipe(gulp.dest('src/css/css_min/'));
+});
+//------------------------------------------
+
+//-----------gulp-control-version-----------
+gulp.task('v-concat_js_1', ['cssnano'], function () { 
+  conver.rewrite({
+                resdir: "basic/components/widgets/assets/css/",
+                namefile: "-general_style-min.сss", 
+                dirrefactor: ["basic/assets/", ["NavbarAsset.php"]]
+              });
+//------------------------------------------
+```
+
+## 3
+
+```javascript
+basic/components/widgets/assets/css/313787045-general_style-min.сss
+```
+
+## 4
+
+NavbarAsset.php
+
+```javascript
+class NavbarAsset extends AssetBundle
+{
+    public $sourcePath = '@app/components/widgets/assets';
+    public $css = [
+        'css/313787045-general_style-min',
+    ];
+    public $js = [
+        'js/lib/jquery.color-2.1.0.min.js',
+        'js/317260467-navbar.js'
+    ];
+}
+
+```
+## API
+
+### rewrite({resdir, namefile , dirrefactor, namerefactor , codname})
++ resdir
+
+  Resource file directory
+
++ namefile
+
+  Resource file name
+
++ dirrefactor[url,[filename]]
+
+    +  url
+
+      File directory that connects resources
+
+      dirrefactor: ["URL", ["NavbarAsset.php"]]
+
+      Exmample: dirrefactor: ["basic/assets/", ["NavbarAsset.php"]]
+
+    +  filename
+
+      The file name in which you want to rename the resource name
+
++ namerefactor
+
+  + namerefactor = namefile ('Default')
+
+    Resource file name, which will be connected
+
++ codname
+
+  + codname = "utf-8" ('Default')
+
+    Encoding
+
+
+
+## Bugs
+
+* Please to write
+
+  * github - [github.com/semiromid/gulp-update-modul](https://github.com/semiromid/gulp-control-version) 
+
+  * email - xor.wind@mail.ru
+ 
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
--- index.js
+++ index.js
@@ -1,111 +1,143 @@
 'use strict';
-
 var  fs = require('fs'),
-     Stream = require('stream'),
-     glo_name,
      adler32 = require('adler32');
 
 
+var tew = function (dname, fname) {
+var readable;
 
+  testfilename(dname, fname);
+  function testfilename(dir, namefile) {
 
-var tew = function (wayfile) {
+    fs.readdir(dir, function(err, items) {
+      for (var i=0; i<items.length; i++) {
+        if(items[i].substring(9, 50) ===  fname){
 
-console.error("NEW TEST - "+wayfile);
-
-
-watchfs();
-function watchfs(){
-var w = fs.watch(wayfile, function (event, filename) {
-    console.log('event is: ' + event);
-       if (filename) {
-         console.log('filename provided: ' + filename);
-         glo_name = filename;
-w.close(); 
-         ter();
-       } else {
-         console.log('filename not provided');
-       }
-    });
-}
-
-
-
-  
-
-
-function hasfilemane(){
-  var timeInMs = Date.now();
-  var data = new Buffer(''+timeInMs);
-  var sum = adler32.sum(data);
-  console.log('timeInMs 2 : ' + sum);
-  return sum;
-}
-
-
-function ter(){
-var namer ="" + glo_name;
-  //Определяем есть ли в имени файла хэш
-  if(gusorstr(namer) === 10){
-      //console.log("Удаляем первые несколько символов");
-      namer=namer.substring(9);
-      //console.log("Удалили - "+mysay);
-    }
-
-  
-    fs.rename('src/test/'+glo_name+"", 'src/test/'+hasfilemane()+""+namer+"", function(err) {
-    if ( err ) console.log('ERROR: ' + err)
-    });
-   watchfs();
-}
-
-
-function gusorstr(finame) {
-  var a = 0;
-        var b = parseInt(finame[0]);
-        var v = parseInt(finame[1]);
-        for(var i1=0; i1<10; i1++ ){
-          if(b === i1){
-            for(var i2=0; i2<10; i2++ ){
-              if(v === i2){
-                console.log("Это хэш");
-                i2=10;
-                a = 10;
-              }
-            }
-            i1=10;
-          }else if(i1 === 9){
-            console.log("Это слово");
-          }
-
+          fs.unlinkSync(dname+""+items[i]);
         }
-  return a;
+      }
+    });
+  }
+
+
+
+  function hasfilemane2(){
+      var timeInMs = Date.now();
+      var data = new Buffer(''+timeInMs);
+      var sum = adler32.sum(data);
+    return sum;
+  }
+var inte2 = hasfilemane2();
+readable = inte2;
+
+  return readable;
 }
+module.exports = exports = tew;
 
 
 
-var readable = new Stream.Transform({ objectMode: true });
-    readable._transform = function (file, unused, callback) {
 
-    //console.log(colors.red('This version will be removed. Download the new version - https://www.npmjs.com/package/gulp-update-module'));
+exports.rewrite = function (data){
+var g_hash;
 
-    
-     // [START]
-     //s------------------------------------------------------------------
-     
-    
-  
-    //s------------------------------------------------------------------
-    
+  var str = JSON.stringify(data);
+  var data_json = JSON.parse(str);
+
+  var resdir = data_json.resdir;
+  var namefile = data_json.namefile;
+  var dirrefactor = data_json.dirrefactor;
+  var namerefactor = data_json.namerefactor;
+  var codname = data_json.codname;
+
+  var dirrefactorURL = data.dirrefactor[0];
 
 
-        
-        //....
-        //
-        // Will be updated
-        //
-        //....
-       return readable;
-    }
-    return readable;
-}
-module.exports = tew;
+  if(codname === undefined){
+    codname = "utf-8";
+  }
+  if(namerefactor === undefined ){
+    namerefactor = namefile;
+  }
+
+
+
+  fs.readdir(resdir, function(err, items) {
+  //fs--------------------------------------------------  
+    if (!err) {
+      for (var i=0; i<items.length; i++) {
+        //for-------------------------------------------
+        if(items[i].substring(9, 50) ===  namefile){
+         g_hash = items[i].substring(0, 9);
+         
+
+          for(var i=0; data.dirrefactor[1].length > i; i++){
+            //for-2-------------------------------------
+              var url = data.dirrefactor[0]+""+data.dirrefactor[1][i];
+
+              //[read]
+              var datafile = fs.readFileSync(url, codname);
+              //fs-rename-----------------------------------
+                  var renameMe = namerefactor;
+                  var re = RegExp("\\d{9}"+renameMe , "g");
+                  datafile = datafile.replace(re, g_hash+""+namerefactor);
+
+                  fs.writeFileSync(url, datafile);
+                  datafile = null;
+              //fs-rename-----------------------------------
+              //[/read]
+          //for-2-------------------------------------
+          }
+        }  
+      }
+      //for-------------------------------------------------
+    }else{
+      return console.log(err);
+    }    
+  //fs-------------------------------------------------- 
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
